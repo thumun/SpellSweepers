ce192889c2b775880cdf394a05653ed24f343651
Neha Thumu <71038542+thumun@users.noreply.github.com>
2024-11-13 23:22:39 -0500

    initial commit

---
+++ Assets/GestureRecognition/GestureManager/Scripts/SubmenuFileSuggestions.cs
@@ -0,0 +1,135 @@
+/*
+ * MiVRy - 3D gesture recognition library plug-in for Unity.
+ * Version 2.11
+ * Copyright (c) 2024 MARUI-PlugIn (inc.)
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SubmenuFileSuggestions : MonoBehaviour
+{
+    private bool initialized = false;
+    TextMesh textField;
+    GameObject upButton;
+    GameObject selectButton;
+    GameObject downButton;
+    GameObject background;
+
+    // File/folder suggestions for the load files button
+    public static int file_suggestion_i = 0;
+    public static List<string> file_suggestions = new List<string>();
+    public static EditableTextField active_text_field = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void init()
+    {
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            GameObject child = this.transform.GetChild(i).gameObject;
+            switch (child.name)
+            {
+                case "SubmenuFilesSuggestionsText":
+                    this.textField = child.GetComponent<TextMesh>();
+                    break;
+                case "SubmenuFilesSuggestionsUpBtn":
+                    this.upButton = child;
+                    break;
+                case "SubmenuFilesSuggestionsSelectBtn":
+                    this.selectButton = child;
+                    break;
+                case "SubmenuFilesSuggestionsDownBtn":
+                    this.downButton = child;
+                    break;
+                case "SubmenuFilesSuggestionsBackground":
+                    this.background = child;
+                    break;
+            }
+        }
+        this.initialized = true;
+    }
+
+    public void refresh()
+    {
+        if (!this.initialized)
+            this.init();
+        GestureManager gm = GestureManagerVR.me?.gestureManager;
+        if (gm == null)
+            return;
+        file_suggestions.Clear();
+        string currentPath = null;
+        if (active_text_field?.target == EditableTextField.Target.LoadFile) {
+            currentPath = gm.gr != null ? gm.fileLoadGestures : gm.fileLoadCombinations;
+            currentPath = gm.getLoadPath(currentPath);
+        } else if (active_text_field?.target == EditableTextField.Target.SaveFile) {
+            currentPath = gm.gr != null ? gm.fileSaveGestures : gm.fileSaveCombinations;
+            currentPath = gm.getSavePath(currentPath);
+        } else {
+            this.gameObject.SetActive(false);
+            return;
+        }
+        string currentDir = Path.GetDirectoryName(currentPath);
+        if (!Directory.Exists(currentDir)) {
+            this.gameObject.SetActive(false);
+            return;
+        }
+        foreach(string f in Directory.GetDirectories(currentDir)) {
+            file_suggestions.Add(Path.GetFileName(f) + "/");
+        }
+        foreach(string f in Directory.GetFiles(currentDir, "*.dat")) {
+            file_suggestions.Add(Path.GetFileName(f));
+        }
+        if (file_suggestions.Count == 0) {
+            this.gameObject.SetActive(false);
+            return;
+        }
+        this.gameObject.SetActive(true);
+        this.background.SetActive(true);
+        if (!selectButton.activeSelf) selectButton.SetActive(true);
+        if (file_suggestion_i < 0) {
+            file_suggestion_i = 0;
+        } else if (file_suggestion_i >= file_suggestions.Count) {
+            file_suggestion_i = file_suggestions.Count - 1;
+        }
+        textField.text = "";
+        if (file_suggestion_i == 0) {
+            textField.text += "\n";
+            upButton.SetActive(false);
+        } else {
+            textField.text += file_suggestions[file_suggestion_i - 1] + "\n";
+            if (!upButton.activeSelf) upButton.SetActive(true);
+        }
+        textField.text += file_suggestions[file_suggestion_i] + "\n";
+        if (file_suggestion_i + 1 < file_suggestions.Count) {
+            textField.text += file_suggestions[file_suggestion_i + 1];
+            if (!downButton.activeSelf) downButton.SetActive(true);
+        } else {
+            downButton.SetActive(false);
+        }
+    }
+}
---
+++ Assets/GestureRecognition/GestureManager/Scripts/SubmenuFilesButton.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * MiVRy - 3D gesture recognition library plug-in for Unity.
+ * Version 2.11
+ * Copyright (c) 2024 MARUI-PlugIn (inc.)
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SubmenuFilesButton : GestureManagerButton
+{
+    [System.Serializable]
+    public enum Operation {
+        LoadGestureFile,
+        SaveGestureFile,
+        FileSuggestionUp,
+        FileSuggestionDown,
+        FileSuggestionSelect
+    };
+    public Operation operation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GestureManager gm = GestureManagerVR.me?.gestureManager;
+        if (!other.name.EndsWith("pointer") || gm == null)
+            return;
+        if (GestureManagerVR.isGesturing)
+            return;
+        if (GestureManagerVR.activeButton != null)
+            return;
+        GestureManagerVR.activeButton = this;
+        this.material = activeButtonMaterial;
+        switch (this.operation) {
+            case Operation.LoadGestureFile:
+                gm.loadFromFile();
+                break;
+            case Operation.SaveGestureFile:
+                gm.saveToFile();
+                break;
+            case Operation.FileSuggestionUp:
+                if (SubmenuFileSuggestions.file_suggestion_i > 0) {
+                    SubmenuFileSuggestions.file_suggestion_i--;
+                }
+                break;
+            case Operation.FileSuggestionDown:
+                if (SubmenuFileSuggestions.file_suggestion_i + 1 < SubmenuFileSuggestions.file_suggestions.Count) {
+                    SubmenuFileSuggestions.file_suggestion_i++;
+                }
+                break;
+            case Operation.FileSuggestionSelect:
+                if (SubmenuFileSuggestions.file_suggestion_i >= 0 && SubmenuFileSuggestions.file_suggestion_i < SubmenuFileSuggestions.file_suggestions.Count) {
+                    if (SubmenuFileSuggestions.active_text_field?.target == EditableTextField.Target.LoadFile) {
+                        if (gm.gr != null) {
+                            string dir = (gm.fileLoadGestures.Contains("/") || gm.fileLoadGestures.Contains("\\"))
+                                       ? Path.GetDirectoryName(gm.fileLoadGestures).Replace('\\', '/') + "/"
+                                       : "";
+                            gm.fileLoadGestures = dir + SubmenuFileSuggestions.file_suggestions[SubmenuFileSuggestions.file_suggestion_i];
+                        } else if (gm.gc != null) {
+                            string dir = (gm.fileLoadCombinations.Contains("/") || gm.fileLoadCombinations.Contains("\\"))
+                                       ? Path.GetDirectoryName(gm.fileLoadCombinations).Replace('\\', '/') + "/"
+                                       : "";
+                            gm.fileLoadCombinations = dir + SubmenuFileSuggestions.file_suggestions[SubmenuFileSuggestions.file_suggestion_i];
+                        }
+                    } else if (SubmenuFileSuggestions.active_text_field?.target == EditableTextField.Target.SaveFile) {
+                        if (gm.gr != null) {
+                            string dir = (gm.fileSaveGestures.Contains("/") || gm.fileSaveGestures.Contains("\\"))
+                                       ? Path.GetDirectoryName(gm.fileSaveGestures).Replace('\\', '/') + "/"
+                                       : "";
+                            gm.fileSaveGestures = dir + SubmenuFileSuggestions.file_suggestions[SubmenuFileSuggestions.file_suggestion_i];
+                        } else if (gm.gc != null) {
+                            string dir = (gm.fileSaveCombinations.Contains("/") || gm.fileSaveCombinations.Contains("\\"))
+                                       ? Path.GetDirectoryName(gm.fileSaveCombinations).Replace('\\', '/') + "/"
+                                       : "";
+                            gm.fileSaveCombinations = dir + SubmenuFileSuggestions.file_suggestions[SubmenuFileSuggestions.file_suggestion_i];
+                        }
+                    }
+                }
+                break;
+        }
+        GestureManagerVR.setInputFocus(null);
+        GestureManagerVR.refresh();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name.EndsWith("pointer") && (Object)GestureManagerVR.activeButton == this)
+            GestureManagerVR.activeButton = null;
+        this.material = inactiveButtonMaterial;
+    }
+
+    private void OnDisable()
+    {
+        if ((Object)GestureManagerVR.activeButton == this)
+            GestureManagerVR.activeButton = null;
+        this.material = inactiveButtonMaterial;
+    }
+}
